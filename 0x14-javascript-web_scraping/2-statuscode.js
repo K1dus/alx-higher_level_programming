c4f4b98d083fc89564560f03e0f2fa828edc84f3
K1dus <88451528+K1dus@users.noreply.github.com>
2022-04-12 23:19:15 +0300

    Create 2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (error, response) {
+
+  if (error) {
+
+    console.error(error);
+
+  }
+
+  console.log('code:', response && response.statusCode);
+
+});
